68cf5f1db18b4a0840e29af512cd2f8ba01c1859
Brandon Viertel <bviertel@gmail.com>
2017-04-13 08:52:13 -0400

    ADDED: Filled list with games

---
+++ Games List/Game.swift
@@ -0,0 +1,23 @@
+//
+//  Game.swift
+//  Games List
+//
+//  Created by Brandon Viertel on 4/13/17.
+//  Copyright © 2017 Brandon. All rights reserved.
+//
+
+// import Foundation
+
+class Game {
+    
+    var name : String = ""
+    
+    var type : String = ""
+    
+    var rating : Int = 0
+    
+    // Variable for 'Need to buy?'
+    
+    var nTB : Bool = false
+    
+}
---
+++ Games List/GamesViewController.swift
@@ -0,0 +1,89 @@
+//
+//  GamesViewController.swift
+//  Games List
+//
+//  Created by Brandon Viertel on 4/13/17.
+//  Copyright © 2017 Brandon. All rights reserved.
+//
+
+import UIKit
+
+class GamesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    // Variable for the Main Table View
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var games : [Game] = []
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        games = createGames()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    // Returns the number of rows in table based on number of entries in array.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return games.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        
+        let game = games[indexPath.row]
+        
+        // Changes String of each 'cell' on Main Table View to game.name
+        
+        cell.textLabel!.text = game.name
+        
+        return cell
+        
+    
+    }
+    
+    // Creates an initial list of games
+    
+    func createGames() -> [Game] {
+        
+        let game1 = Game()
+        
+        game1.name = "World of Warcraft"
+        
+        game1.type = "MMORPG"
+        
+        game1.rating = 7
+        
+        game1.nTB = false
+        
+        let game2 = Game()
+        
+        game2.name = "Legend of Zelda"
+        
+        game2.type = "Adventure"
+        
+        game2.rating = 10
+        
+        game2.nTB = true
+        
+        let gA : [Game] = [game1,game2]
+        
+        return gA
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
